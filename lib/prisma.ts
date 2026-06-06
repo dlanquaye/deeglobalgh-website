7582ebeaedf795048bcc0d891a47cef199a01666
Dolores Lanquaye <lanquayedolores2@gmail.com>
2026-06-06 22:29:56 +0000

    Use recovery database override

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+const databaseUrl =
+  process.env.DATABASE_URL_RECOVERY || process.env.DATABASE_URL;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!(global as any).prisma) {
-    (global as any).prisma = new PrismaClient();
-  }
-  prisma = (global as any).prisma;
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: databaseUrl,
+      },
+    },
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-
-export { prisma };
