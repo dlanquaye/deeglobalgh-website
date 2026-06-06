2415f9f8b0066adf83e224985e85aaa76061891d
Dolores Lanquaye <lanquayedolores2@gmail.com>
2026-06-06 23:17:20 +0000

    Remove datasource override

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-const databaseUrl =
-  process.env.DATABASE_URL_RECOVERY || process.env.DATABASE_URL;
-
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: databaseUrl,
-      },
-    },
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
